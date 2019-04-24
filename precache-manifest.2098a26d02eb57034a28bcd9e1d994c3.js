7d95fe81e030cd3b83da194006c7b4b79e5c35df
harshika <hgupta@cainc.com>
2019-04-24 16:08:41 +0530

    Updates

---
+++ precache-manifest.2098a26d02eb57034a28bcd9e1d994c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8eb2e6d1287443fc53",
+    "url": "/Harshika-g/Avatar-app/static/js/runtime~main.e93cbf13.js"
+  },
+  {
+    "revision": "c324d4a76cc78aa00ae4",
+    "url": "/Harshika-g/Avatar-app/static/js/main.6e27019d.chunk.js"
+  },
+  {
+    "revision": "b1e035acb082398f1f90",
+    "url": "/Harshika-g/Avatar-app/static/js/2.3e811095.chunk.js"
+  },
+  {
+    "revision": "c324d4a76cc78aa00ae4",
+    "url": "/Harshika-g/Avatar-app/static/css/main.aaf7d7da.chunk.css"
+  },
+  {
+    "revision": "b1e035acb082398f1f90",
+    "url": "/Harshika-g/Avatar-app/static/css/2.86e5aaf1.chunk.css"
+  },
+  {
+    "revision": "0812aed46011345d4d1a9f421186e323",
+    "url": "/Harshika-g/Avatar-app/index.html"
+  }
+];
